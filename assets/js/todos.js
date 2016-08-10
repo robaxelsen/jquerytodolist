2b729878fda0516d6eb3f5c4c812f3bd07a2806e
robeerob <rob@rob.ee>
2016-08-10 10:29:19 +0200

    func: add delete todo functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $('li').on('click', function() {
     $(this).toggleClass('completed');
 });
+
+// Click on X to delete Todo
+$('span').click( function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
