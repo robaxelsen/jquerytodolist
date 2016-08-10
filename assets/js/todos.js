84115fc5c64282b281815e9fd7969f6624927781
robeerob <rob@rob.ee>
2016-08-10 10:41:58 +0200

    func: add new todo functionality, and change event listeners to .on method

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check Off Specific Todos By Clicking
-$('li').on('click', function() {
+$('ul').on('click', 'li', function() {
     $(this).toggleClass('completed');
 });
 
 // Click on X to delete Todo
-$('span').click( function(event) {
+$('ul').on('click', 'span', function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$('input[type="text"]').keypress(function(event) {
+    if (event.which === 13) {
+        // catch new todo text from input field
+        todoText = $(this).val();
+        $(this).val('');
+        // create a new li and add to ul
+        $('ul').append('<li><span>X</span> ' + todoText + '</li>');
+    };
 });
